e45c73f47391627ddb64457a230b304e54916186
nmlpx09 <nmlpx09@duck.com>
2025-10-12 19:22:01 +0300

    fix

--- pulse-client/config.h
+++ pulse-client/config.h
@@ -9,7 +9,7 @@ static const std::uint16_t PORT = 1234;
 static const std::size_t DATASIZE = 3840;
 
 //pulse
-static const std::string DEVICE = "openas.monitor";
+static const std::string DEVICE = "opennap.monitor";
 static const std::string FORMAT = "s32le";
 static const std::int8_t CHANNELS = 2;
 static const std::uint32_t RATE = 48000;
